1a1698f4f55415260a589fa61c1cc3981af3d4b3
damiano <damianovisa@gmail.com>
2023-01-17 23:01:21 -0500

    new addProduct page

--- app/views/Seller/addProduct.php
+++ app/views/Seller/addProduct.php
@@ -2,15 +2,18 @@
 <head>
     <title>Add product</title>
      <!-- Bootstrap -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- css -->
-    <link rel="stylesheet" href="/resources/style.css">
+    <link rel="stylesheet" href="../resources/style.css">
+    <!-- Bootstrap ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 </head>
 <body>
     <?php $this->view("/Includes/header");?>
 
-<form class="row g-3 needs-validation" action="" method="post" novalidate>
+<!-- <form class="row g-3 needs-validation" action="" method="post" novalidate>
     <div class="col-md-4">
         <label for="validationCustom01" class="form-label"><?=_("Product name")?></label>
         <input type="text" class="form-control" id="validationCustom01" name="name" required>
@@ -50,7 +53,76 @@
     <div class="col-12">
         <input class="btn btn-primary" name="action" type="submit" href="/Seller/addProduct" value="Add">
     </div>
+</form> -->
+
+<section class="vh-100">
+<form class="needs-validation" action="" method="post" novalidate>
+    <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form>
+                    <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                        <p class="lead fw-normal mb-4 me-3">Add a product</p>
+                    </div>
+
+                    <div class="form-outline mb-4 has-validation" >
+                        <label class="form-label" for="validationCustomUsername" ><?=_("Product name")?></label>
+                        <input type="text" name="name" minlength="1" id="validationCustomUsername" class="form-control form-control-lg"
+                        placeholder="Enter product name" aria-describedby="inputGroupPrepend" required/>
+                        <div class="invalid-feedback">
+                            Must enter a product name
+                        </div>
+                    </div>
+                    <div class="form-outline mb-4 has-validation" >
+                        <label class="form-label" for="validationCustomUsername" ><?=_("Manufacturer")?></label>
+                        <input type="text" name="manufacturer" minlength="1" id="validationCustomUsername" class="form-control form-control-lg"
+                        placeholder="Enter manufacturer" aria-describedby="inputGroupPrepend" required/>
+                        <div class="invalid-feedback">
+                            Must enter a manufacturer
+                        </div>
+                    </div>
+                    <!-- Email input -->
+                    <div class="form-outline mb-4 has-validation" >
+                        <label class="form-label" for="validationCustomUsername" ><?=_("Description")?></label>
+                        <input type="text" name="description" id="validationCustomUsername" class="form-control form-control-lg"
+                        placeholder="Enter description" aria-describedby="inputGroupPrepend" required/>
+                        <div class="invalid-feedback">
+                            Must enter a description
+                        </div>
+                    </div>
+
+                    <!-- Password input -->
+                    <div class="form-outline mb-3">
+                        <label class="form-label" for="validationCustom03"><?=_("Price")?></label>
+                        <input type="text" minlength="1" id="validationCustom03" class="form-control form-control-lg"
+                        placeholder="Price $" name="price" required/>
+                        <div class="invalid-feedback">
+                            Product must have a price
+                        </div>  
+                    </div>
+
+                    <div class="form-outline mb-3">
+                        <label class="form-label" for="validationCustom03"><?=_("Image")?></label>
+                        <input type="file" minlength="1" id="validationCustom03" class="form-control form-control-lg"
+                        name="image" required/>
+                        <div class="invalid-feedback">
+                            
+                        </div>  
+                    </div>
+
+                    <div class="text-center text-lg-start mt-4 pt-2">
+                        <input class="btn btn-primary btn-lg" name="action" type="submit" href="/Seller/addProduct" value="Add">
+                    </div>
+
+                </form>
+                
+            </div>
+             
+        </div>
+    </div>
 </form>
+</section>
 
 <script src="/resources/validation.js"></script>
 
